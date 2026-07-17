602bf54654e86fee42b1e88261e3daaac1f391ea
Nazmul Hossain <nazmulhossain664@gmail.com>
2026-07-18 01:52:35 +0600

    fix: update relative import path for purchase_receipt module in stock_entry hooks

--- offline_pos/offline_pos/hooks_methods/stock_entry.py
+++ offline_pos/offline_pos/hooks_methods/stock_entry.py
@@ -1,6 +1,6 @@
 import frappe
 
-from purchase_receipt import assign_batch, process_batch
+from . purchase_receipt import assign_batch, process_batch
 
 def validate(doc, method="None"):
     process_batch(doc)
